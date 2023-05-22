02737b4879b052c4ff05e5227efa179fe8d17d8c
seryegas <sergeu.shakirov@gmail.com>
2023-05-22 18:14:05 +0300

    Исправления

--- autoloader.php
+++ autoloader.php
@@ -1,5 +1,6 @@
 <?php
 
 spl_autoload_register(function ($class_name) {
+    $class_name = str_replace('\\', '/', $class_name);
     include $_SERVER['DOCUMENT_ROOT'] . '/src/' . __NAMESPACE__ . '/' . $class_name . '.php';
 });
